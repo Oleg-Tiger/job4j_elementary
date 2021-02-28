1a507a3233060c21bbb7510a8bc2c4cc2f976520
Oleg-Tiger <free_person@inbox.ru>
2021-02-28 22:27:38 +0300

    4.6.2. Switch - matcher [#480738]

--- src/ru/job4j/Main.java
+++ src/ru/job4j/Main.java
@@ -1,10 +1,8 @@
 package ru.job4j;
 
-import ru.job4j.condition.MultipleSwitchWeek;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println(new MultipleSwitchWeek().numberOfDay("Понедельник"));
+
     }
 }
