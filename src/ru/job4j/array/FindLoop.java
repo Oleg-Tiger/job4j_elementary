29c0d33718500b71841118ed09d44aed8913fb9a
Oleg-Tiger <free_person@inbox.ru>
2021-03-05 16:28:38 +0300

    6.5. Поиск индекса в диапазоне. [#480755]

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -1,8 +1,9 @@
 package ru.job4j.array;
 
 public class FindLoop {
+
     public static int indexOf(int[] data, int el) {
-        int rst = -1; /* если элемента нет в массиве, то возвращаем -1. */
+        int rst = -1;
         for (int index = 0; index < data.length; index++) {
             if (data[index] == el) {
                 rst = index;
@@ -10,5 +11,16 @@ public class FindLoop {
             }
         }
         return rst;
+    }
+
+    public static int indexOf(int[] data, int el, int start, int finish) {
+        int rst = -1;
+        for (int index = start; index <= finish; index++) {
+            if (data[index] == el) {
+                rst = index;
+                break;
+            }
+        }
+    return rst;
     }
 }
--- test/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/array/FindLoopTest.java
@@ -32,5 +32,27 @@ public class FindLoopTest {
         int expect = 2;
         assertThat(result, is(expect));
     }
+
+    @Test
+    public void whenFind2Then3() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenDoNotFind4ThenMinus1() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 4;
+        int start = 0;
+        int finish = 3;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
 }
 
