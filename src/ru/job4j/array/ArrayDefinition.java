48d9f439018c1ac319b53677c98b2f6b1872be51
Oleg-Tiger <free_person@inbox.ru>
2021-03-08 22:43:49 +0300

    1. Чтение кода и базовые ошибки [#480920]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,17 +5,14 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
-
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
-
         String[] names = new String[4];
         names[0] = "Petr Arsentev";
         names[1] = "Oleg Ershov";
         names[2] = "Ivan Ivanov";
         names[3] = "Andrey Petrov";
-
         for (int i = 0; i < names.length; i++) {
             System.out.println(names[i]);
         }
